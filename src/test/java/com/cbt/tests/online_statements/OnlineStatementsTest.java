c4485960ea5e56cae4323b8b4708b98a68dbe65b
vadymkotliar <kote4k@gmail.com>
2018-07-30 23:36:57 -0400

    TC017 and TC019 are ready

---
+++ src/test/java/com/cbt/tests/online_statements/OnlineStatementsTest.java
@@ -0,0 +1,32 @@
+package com.cbt.tests.online_statements;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import com.cbt.pages.HomePage;
+import com.cbt.pages.LoginPage;
+import com.cbt.pages.OnlineStatements;
+import com.cbt.tests.TestBase;
+import com.cbt.utilities.ConfigurationReader;
+
+public class OnlineStatementsTest extends TestBase {
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
+    OnlineStatements onlineStatements = new OnlineStatements();
+
+    @Test
+    public void TC019_StatementPositive() {
+	homePage.signin.click();
+	loginPage.login(ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
+
+	onlineStatements.onlineStatementsTab.click();
+	Select select = new Select(onlineStatements.accountDropdown);
+	select.selectByVisibleText("Checking");
+
+	assertTrue(onlineStatements.year2009.isDisplayed(), "Year 2009 is not displayed");
+	onlineStatements.year2009.click();
+
+    }
+}
---
+++ src/test/java/com/cbt/tests/pay_bills/PayBillsTest.java
@@ -0,0 +1,39 @@
+package com.cbt.tests.pay_bills;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import com.cbt.pages.HomePage;
+import com.cbt.pages.LoginPage;
+import com.cbt.pages.PayBillsPage;
+import com.cbt.tests.TestBase;
+import com.cbt.utilities.ConfigurationReader;
+
+public class PayBillsTest extends TestBase {
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
+    PayBillsPage payBillsPage = new PayBillsPage();
+
+    @Test
+    public void TC017_PayBillsNegative() {
+	homePage.signin.click();
+	loginPage.login(ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
+
+	payBillsPage.payBillsTab.click();
+	Select select = new Select(payBillsPage.payeeDropdown);
+	select.selectByValue("wellsfargo");
+
+	select = new Select(payBillsPage.accountDropdown);
+	select.selectByValue("5");
+	payBillsPage.date.sendKeys("2018-07-16");
+	payBillsPage.description.sendKeys("default decription");
+	payBillsPage.payButton.click();
+
+	String expected = "Make payments to your saved payees";
+	String actual = payBillsPage.makePaymentsToYourSavedpayees.getText();
+	assertEquals(actual, expected, "Text does not match");
+
+    }
+}
