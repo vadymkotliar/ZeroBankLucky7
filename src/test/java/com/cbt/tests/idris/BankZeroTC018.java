a92c90df17e4a448f42e31397d9dadd12c496403
CybertekSchool <github@cybertekschool.com>
2018-07-31 18:00:05 -0400

    add TC018 OnLine Statement

---
+++ src/test/java/com/cbt/tests/idris/BankZeroTC018.java
@@ -0,0 +1,35 @@
+package com.cbt.tests.idris;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.cbt.pages.HomePage;
+import com.cbt.pages.LoginPage;
+import com.cbt.tests.TestBase;
+import com.cbt.utilities.ConfigurationReader;
+import com.cbt.utilities.Driver;
+
+public class BankZeroTC018 extends TestBase{
+
+
+	HomePage homePage = new HomePage();
+	LoginPage loginPage = new LoginPage();
+	
+	@Test()
+	public void SPA186() {
+		homePage.signin.click();
+		
+		String userName = ConfigurationReader.getProperty("username");
+		String password = ConfigurationReader.getProperty("password");
+		loginPage.login(userName, password);
+		
+		homePage.onlineStatements.click();
+		
+		String expected = "Zero - Online Statements";
+		String actual = Driver.getDriver().getTitle();
+		assertEquals(actual, expected);
+		
+	}
+	
+}
